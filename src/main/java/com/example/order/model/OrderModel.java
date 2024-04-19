d000dd26a5c2c67fcf92956572124158cf2cdedb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-19 22:36:15 +0200

    OrderModel

--- src/main/java/com/example/order/model/OrderModel.java
+++ src/main/java/com/example/order/model/OrderModel.java
@@ -4,18 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.time.LocalDateTime;
-
+@Data
 @Entity
 @EnableFeignClients
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class OrderModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Integer orderNumber;
+    private String orderNumber;
     private LocalDateTime deliveryTime;
 }
