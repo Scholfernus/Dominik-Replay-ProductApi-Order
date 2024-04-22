48266efc6a0731b0562fa367a74fa053585f6efb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-22 23:31:33 +0200

    OrderService

--- src/main/java/com/example/order/service/OrderService.java
+++ src/main/java/com/example/order/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.example.order.service;
 
+import com.example.order.dto.OrderDto;
 import com.example.order.model.OrderModel;
 import com.example.order.repository.OrderRepository;
+import com.example.order.utils.OrderMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,20 @@ public class OrderService {
 
     public List<OrderModel> getListOfOrders() {
         return orderRepository.findAll();
+    }
+
+    public OrderModel getOrderById(Long id) {
+    return orderRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public OrderDto addOrder(OrderDto orderDto) {
+        OrderModel orderModel = OrderMapper.toOrderModel(orderDto);
+        OrderModel saved = orderRepository.save(orderModel);
+        return OrderMapper.toOrderDto(saved);
+    }
+
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
     }
 }
 
