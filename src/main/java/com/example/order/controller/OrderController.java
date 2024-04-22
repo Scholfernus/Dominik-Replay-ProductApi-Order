818f7357702575051a4f1eadec8fc199a24e16d6
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-22 23:31:08 +0200

    OrderController

--- src/main/java/com/example/order/controller/OrderController.java
+++ src/main/java/com/example/order/controller/OrderController.java
@@ -1,12 +1,12 @@
 package com.example.order.controller;
 
+import com.example.order.dto.OrderDto;
 import com.example.order.model.OrderModel;
 import com.example.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +23,32 @@ public class OrderController {
             return ResponseEntity.ok(orders);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderModel> getOrderById(@PathVariable("id") Long id) {
+        OrderModel model = orderService.getOrderById(id);
+        return ResponseEntity.ok(model);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDto> postAddOrder(@RequestBody OrderDto orderDto) {
+        try {
+            OrderDto addedOrder = orderService.addOrder(orderDto);
+            return ResponseEntity.ok(addedOrder);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<OrderModel> deleteOrderById(@PathVariable("id") Long id) {
+        try {
+            orderService.deleteOrder(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
         }
     }
 }
